aafa0f7d5557b8dacd8cc5ffe8e7bfa1ef148edf
Amy <amy@amogus.cloud>
2025-05-23 19:37:39 -0400

    wip: add function to check adjacent mines

--- limine.asd
+++ limine.asd
@@ -1,5 +1,7 @@
 (asdf:defsystem "limine"
   :components ((:module "src"
-		:components ((:file "main"))))
+		:components ((:file "main"
+			      :depends-on ("macros"))
+			     (:file "macros"))))
   :version "0.0.1"
   :licence "MIT")
--- src/main.lisp
+++ src/main.lisp
@@ -27,16 +27,25 @@
 
 (defun check-adjacent-cells (board cell)
   "Check adjacent cells for mines, returns the total mine count for this cell."
-)
+  (let ((dx '(-1 -1 -1 0 0 1 1 1))
+	(dy '(-1 0 1 -1 1 -1 0 1))
+	(mine-count 0))
+    (loop for d from 0 below 8
+	  do (let ((row (+ (first cell) (nth d dx)))
+		   (col (+ (second cell) (nth d dy))))
+	       (if (valid-cell-p `(,row ,col) board)
+		   (if (aref board row col)
+		       (setf mine-count (1+ mine-count))))))
+    mine-count))
 
 (defun generate-board (field)
   "Given an already generated minefield, add surrounding clues and return a board.")
 
 (in-package :limine)
 
-(defparameter *difficulties* '((beginner . '(9 10))
-			       (intermediate . '(16 40))
-			       (advanced . '(24 99)))
+(defparameter *difficulties* '((beginner . (9 10))
+			       (intermediate . (16 40))
+			       (advanced . (24 99)))
   "LiMine difficulty alist.")
 
 (defun main ()
