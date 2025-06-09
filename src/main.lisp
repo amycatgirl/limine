45797012ac100977b16e43201080fc992e18c6e3
Amy <amy@amogus.cloud>
2025-06-08 21:16:52 -0400

    feat: reveal all adjacent blank tiles

--- src/main.lisp
+++ src/main.lisp
@@ -5,38 +5,50 @@
 	      "Later gets populated by draw-minefield, contains buttons and their respective coordinates.")
 
 (defparameter *styles* `(,(make-style cell-blue (:extend "TButton")
-				      :foreground (nodgui.utils:make-tk-color :blue))
-			 ,(make-style cell-green (:extend "TButton")
-				      :foreground (nodgui.utils:make-tk-color :darkgreen))
-			 ,(make-style cell-red (:extend "TButton")
-				      :foreground (nodgui.utils:make-tk-color :red))
-			 ,(make-style cell-darkblue (:extend "TButton")
-				      :foreground (nodgui.utils:make-tk-color :darkblue))
-			 ,(make-style cell-darkred (:extend "TButton")
-				      :foreground (nodgui.utils:make-tk-color :darkred))
-			 ,(make-style cell-cyan (:extend "TButton")
-				      :foreground (nodgui.utils:make-tk-color :cyan))
-			 ,(make-style cell-purple (:extend "TButton")
-				      :foreground (nodgui.utils:make-tk-color :purple))
-			 ,(make-style cell-grey (:extend "TButton")
-				      :foreground (nodgui.utils:make-tk-color :grey)))
-	      "List of all possible tile foregrounds.")
+                                      :foreground (nodgui.utils:make-tk-color :blue)
+                                      :font "Monospace 10 bold")
+                         ,(make-style cell-green (:extend "TButton")
+                                      :foreground (nodgui.utils:make-tk-color :darkgreen)
+                                      :font "Monospace 10 bold")
+                         ,(make-style cell-red (:extend "TButton")
+                                      :foreground (nodgui.utils:make-tk-color :red)
+                                      :font "Monospace 10 bold")
+                         ,(make-style cell-darkblue (:extend "TButton")
+                                      :foreground (nodgui.utils:make-tk-color :darkblue)
+                                      :font "Monospace 10 bold")
+                         ,(make-style cell-darkred (:extend "TButton")
+                                      :foreground (nodgui.utils:make-tk-color :darkred)
+                                      :font "Monospace 10 bold")
+                         ,(make-style cell-cyan (:extend "TButton")
+                                      :foreground (nodgui.utils:make-tk-color :cyan)
+                                      :font "Monospace 10 bold")
+                         ,(make-style cell-purple (:extend "TButton")
+                                      :foreground (nodgui.utils:make-tk-color :purple)
+                                      :font "Monospace 10 bold")
+                         ,(make-style cell-grey (:extend "TButton")
+                                      :foreground (nodgui.utils:make-tk-color :grey)
+                                      :font "Monospace 10 bold")
+			 ,(make-style cell-empty (:extend "TButton")
+				      :overrelief :sunken
+				      :relief :sunken))
+  "List of all possible tile foregrounds.")
 
 (defparameter *tile-colour-alist*
-	      '((1 . cell-blue)
-		(2 . cell-green)
-		(3 . cell-red)
-		(4 . cell-darkblue)
-		(5 . cell-darkred)
-		(6 . cell-cyan)
-		(7 . cell-purple)
-		(8 . cell-grey)))
+  '((0 . cell-empty)
+    (1 . cell-blue)
+    (2 . cell-green)
+    (3 . cell-red)
+    (4 . cell-darkblue)
+    (5 . cell-darkred)
+    (6 . cell-cyan)
+    (7 . cell-purple)
+    (8 . cell-grey)))
 
 
 (defparameter *difficulties* '((beginner . (9 10))
-			       (intermediate . (16 40))
-			       (advanced . (24 99)))
-	      "limine difficulty alist.")
+                               (intermediate . (16 40))
+                               (advanced . (24 99)))
+  "limine difficulty alist.")
 
 (defparameter *dx* '(-1 -1 -1 0 0 1 1 1)
 	      "Adjacent tiles on the x axis")
@@ -82,17 +94,23 @@
 		       (setf mine-count (1+ mine-count))))))
     mine-count))
 
+(defparameter *debug-call-deepness* 0
+  "How deep a call is going.")
+
 (defun check-blank-neighbours (field board cell)
   "Check adjacent blank tiles nearby the given cell. Recursive.
    Returns a bitmap of tiles that need to be opened"
+  (setf *debug-call-deepness* (1+ *debug-call-deepness*))
   (let ((cells-nearby '()))
     (loop for d from 0 below 8
 	  do (let ((row (+ (first cell) (nth d *dx*)))
 		   (col (+ (second cell) (nth d *dy*))))
 	       (if (valid-cell-p `(,row ,col) board)
 		   (cond
-		    ((numberp (aref board row col))
-		     (fire-event (aref *tile-storage* row col) #$<ButtonPress-1>$))
+		     ((not (aref *tile-storage* row col)) (format t "Cell (~a, ~a) already revealed..." row col)) ;; Tile was already revealed, skip.
+		     ((numberp (aref board row col))
+		      (if (aref *tile-storage* row col)
+			  (cell-action field (aref *tile-storage* row col) `(,row ,col) board)))
 		    ((format t "(~a,~a) is probably a mine" row col))))))))
 
 (defun generate-board-with-diff (diff)
@@ -106,26 +124,37 @@
 
 (defun cell-action (field button coords board)
   "Cell button action. Needs a board to work, lol."
+  (setf *debug-call-deepness* (1+ *debug-call-deepness*))
+  (format t "Running cell action for cell ~A" coords)
+  (if (not button)
+      nil)
   (let ((cell (aref board
 		    (first coords)
 		    (second coords))))
     (cond
-     ((numberp cell) (progn
+      ((numberp cell) (progn
+			(print "Hint.")
 		       (if (not (= cell 0))
 			   (progn
 			     (let ((new-style (cdr (assoc cell *tile-colour-alist*))))
 			       (setf (text button) (format nil "~a" cell))
 			       (style-configure button new-style)))
-			 (progn
-			   (check-blank-neighbours field board coords)
-			   (grid-forget button)))))
+			   (progn
+			     (print "About to go into recursion!")
+			     (print *debug-call-deepness*)
+			     (setf (aref *tile-storage* (first coords) (second coords)) nil)
+			     (print "Removed current tile from storage.")
+			     (style-configure button (cdr (assoc 0 *tile-colour-alist*)))
+			     (configure button :state "disabled")
+			     (check-blank-neighbours field board coords)))))
      (cell (progn
 	     (message-box "You lost :("
 			  "Game Over"
 			  "ok"
 			  "warning"
 			  :parent *tk*)
-	     (exit-nodgui))))))
+	     (exit-nodgui)))))
+  (setf (aref *tile-storage* (first coords) (second coords)) nil))
 
 (defun draw-minefield (board)
   "Draw the current minefield into the screen."
@@ -143,6 +172,7 @@
 					:master field-frame)))
 		 (setf (aref *tile-storage* x y) b)
 		 (setf (command b) (lambda ()
+				     (setf *debug-call-deepness* 0)
 				     (cell-action field-frame b stored-coords board)))))))
 (defun main ()
   (let ((sample-board (generate-board-with-diff 'beginner)))
